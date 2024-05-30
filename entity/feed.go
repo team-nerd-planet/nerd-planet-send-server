fee194d9ed3f0835138acf0b3f324b8c416bb080
Jo YoHan <whdygks210@naver.com>
2024-05-30 19:18:26 +0900

    init project

---
+++ entity/feed.go
@@ -0,0 +1,26 @@
+package entity
+
+import "time"
+
+type CompanySizeType int
+
+const (
+	STARTUP CompanySizeType = iota // 스타트업
+	SMALL                          // 중소기업
+	MEDIUM                         // 중견기업
+	LARGE                          // 대기업
+	FOREIGN                        // 외국계
+)
+
+type Feed struct {
+	ID          uint            `gorm:"column:id;primarykey"`
+	Name        string          `gorm:"column:name;type:varchar;not null"`
+	Title       string          `gorm:"column:title;type:varchar;not null"`
+	Description string          `gorm:"column:description;type:varchar;not null"`
+	Link        string          `gorm:"column:link;type:varchar;not null;unique"`
+	Updated     time.Time       `gorm:"column:updated;type:timestamp;not null"`
+	Copyright   string          `gorm:"column:copyright;type:varchar;not null"`
+	CompanySize CompanySizeType `gorm:"column:company_size;type:int2;not null"`
+	RssID       uint            `gorm:"column:rss_id;type:int8;not null"`
+	Items       []Item          `gorm:"foreignKey:FeedID"`
+}
---
+++ entity/item.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type Item struct {
+	ID          uint       `gorm:"column:id;primarykey"`
+	Title       string     `gorm:"column:title;type:varchar;not null"`
+	Description string     `gorm:"column:description;type:varchar;not null"`
+	Link        string     `gorm:"column:link;type:varchar;not null"`
+	Thumbnail   *string    `gorm:"column:thumbnail;type:varchar"`
+	Published   time.Time  `gorm:"column:published;type:timestamp;not null"`
+	GUID        string     `gorm:"column:guid;type:varchar;not null"`
+	FeedID      uint       `gorm:"column:feed_id;type:int8;not null"`
+	JobTags     []JobTag   `gorm:"many2many:item_job_tags;"`
+	SkillTags   []SkillTag `gorm:"many2many:item_skill_tags;"`
+}
+
+type ItemView struct {
+	ItemID          uint            `gorm:"column:item_id;type:int8"`
+	ItemTitle       string          `gorm:"column:item_title;type:varchar"`
+	ItemDescription string          `gorm:"column:item_description;type:varchar"`
+	ItemLink        string          `gorm:"column:item_link;type:varchar"`
+	ItemThumbnail   *string         `gorm:"column:item_thumbnail;type:varchar"`
+	ItemPublished   time.Time       `gorm:"column:item_published;type:timestamp"`
+	FeedID          uint            `gorm:"column:feed_id;type:varchar"`
+	FeedName        string          `gorm:"column:feed_name;type:varchar"`
+	FeedTitle       string          `gorm:"column:feed_title;type:varchar"`
+	FeedLink        string          `gorm:"column:feed_link;type:varchar"`
+	CompanySize     CompanySizeType `gorm:"column:company_size;type:int2"`
+	JobTagIDArr     pq.Int64Array   `gorm:"column:job_tags_id_arr;type:int8[]"`
+	SkillTagIDArr   pq.Int64Array   `gorm:"column:skill_tags_id_arr;type:int8[]"`
+}
+
+func (ItemView) TableName() string {
+	return "vw_items"
+}
+
+type ItemRepo interface {
+	CountView(company *string, companySizes *[]CompanySizeType, jobTags, skillTags *[]int64) (int64, error)
+	FindAllView(company *string, companySizes *[]CompanySizeType, jobTags, skillTags *[]int64, perPage int, page int) ([]ItemView, error)
+}
