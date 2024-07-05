27668a1b98f99dc012e3b0533742bf056017cfc0
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2024-07-05 18:59:01 +0900

    temp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,9 +12,11 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"text/template"
 	"time"
 
+	"github.com/jasonlvhit/gocron"
 	"github.com/slack-go/slack"
 	"github.com/spf13/viper"
 	"github.com/team-nerd-planet/send-server/entity"
@@ -22,6 +24,29 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+// time parse layout "2006-01-02 15:04:05"
+
+const (
+	preparationHour   int = 6
+	preparationMinute int = 50
+	scheduleHour      int = 7
+	scheduleMinute    int = 0
+)
+
+var (
+	todayScheduleDateTime time.Time
+)
+
+func init() {
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = loc
+	gocron.ChangeLoc(loc)
+}
 
 func main() {
 	conf, err := NewConfig()
@@ -55,55 +80,45 @@ func main() {
 		panic(err)
 	}
 
-	// location, err := time.LoadLocation("Asia/Seoul")
-	// if err != nil {
-	// 	slog.Error("Unfortunately can't load a location", "error", err.Error())
-	// } else {
-	// 	time.Local = location
-	// 	gocron.ChangeLoc(location)
-	// }
+	slog.Info("current time", "time", time.Now())
 
-	// slog.Info("current time", "time", time.Now())
+	gocron.Every(1).Day().At(fmt.Sprintf("%02d:%02d", preparationHour, preparationMinute)).Do(func() {
+		now := time.Now()
+		slog.Info("start preparing", "time", now)
 
-	// gocron.Every(1).Day().At("07:00").Do(func() {
-	// 	slog.Info("start schedule", "time", *location)
+		todayScheduleDateTime = time.Date(now.Year(), now.Month(), now.Day(), scheduleHour, scheduleMinute, 0, 0, time.Local)
+		slog.Info("Today Schedule Date Time", "time", todayScheduleDateTime)
 
-	var subscriptionArr []entity.Subscription
+		var subscriptionArr []entity.Subscription
 
-	if err := db.Find(&subscriptionArr).Error; err != nil {
-		panic(err)
-	}
+		if err := db.Find(&subscriptionArr).Error; err != nil {
+			panic(err)
+		}
 
-	var sb strings.Builder
-	sb.WriteString("안녕하세요! plaa 입니다.\n너드플라넷의 소식을 아래와 같이 전달했어요!!\n\n")
+		var sb strings.Builder
+		sb.WriteString("안녕하세요! plaa 입니다.\n너드플라넷의 소식을 아래와 같이 전달했어요!!\n\n")
 
-	res := make(chan PublishResult, len(subscriptionArr))
+		wg := new(sync.WaitGroup)
+		resChan := make(chan PublishResult, len(subscriptionArr))
 
-	fmt.Printf("subscriptionArr = %d\n", len(subscriptionArr))
+		for _, subscription := range subscriptionArr {
+			wg.Add(1)
+			go publish(resChan, wg, conf, db, subscription)
+		}
 
-	for _, subscription := range subscriptionArr {
-		go publish(res, conf, db, subscription)
-	}
+		wg.Wait()
+		close(resChan)
 
-	var v PublishResult
-	var ok bool
+		for r := range resChan {
+			sb.WriteString(fmt.Sprintf("%s님에게 %d개의 리스트를 보냈어요.\n", r.name, r.count))
+		}
 
-	for v, ok = <-res; ok; v, ok = <-res {
-		fmt.Println("RES!!!!!!")
-		sb.WriteString(fmt.Sprintf("%s님에게 %d개의 리스트를 보냈어요.\n", v.name, v.count))
-	}
+		//if err := webhook(conf, sb.String()); err != nil {
+		//	panic(err)
+		//}
+	})
 
-	// for r := range res {
-	// 	fmt.Println("RES!!!!!!")
-	// 	sb.WriteString(fmt.Sprintf("%s님에게 %d개의 리스트를 보냈어요.\n", r.name, r.count))
-	// }
-
-	// if err := webhook(conf, sb.String()); err != nil {
-	// 	panic(err)
-	// }
-	// })
-
-	// <-gocron.Start()
+	<-gocron.Start()
 }
 
 type PublishResult struct {
@@ -111,7 +126,7 @@ type PublishResult struct {
 	count int
 }
 
-func publish(res chan PublishResult, conf *Config, db *gorm.DB, subscription entity.Subscription) {
+func publish(res chan PublishResult, wg *sync.WaitGroup, conf *Config, db *gorm.DB, subscription entity.Subscription) {
 	var (
 		items []entity.ItemView
 		where = make([]string, 0)
@@ -128,6 +143,7 @@ func publish(res chan PublishResult, conf *Config, db *gorm.DB, subscription ent
 
 	defer func() {
 		res <- PublishResult{name: name, count: count}
+		wg.Done()
 	}()
 
 	where = append(where, "? <= item_published")
@@ -198,6 +214,9 @@ func publish(res chan PublishResult, conf *Config, db *gorm.DB, subscription ent
 		subject := "Subject: 너드플래닛 기술블로그 뉴스레터 \n"
 		mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 		msg := []byte(subject + mime + body.String())
+
+		<-time.After(time.Since(todayScheduleDateTime))
+
 		err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Smtp.Host, conf.Smtp.Port), auth, from, to, msg)
 		if err != nil {
 			slog.Error(err.Error(), "error", err)
@@ -205,11 +224,11 @@ func publish(res chan PublishResult, conf *Config, db *gorm.DB, subscription ent
 		}
 	}
 
-	// subscription.Published = time.Now()
-	// if err := db.Save(subscription).Error; err != nil {
-	// 	slog.Error(err.Error(), "error", err)
-	// 	return
-	// }
+	subscription.Published = time.Now()
+	if err := db.Save(subscription).Error; err != nil {
+		slog.Error(err.Error(), "error", err)
+		return
+	}
 }
 
 func getArrToString(arr []int64) string {
